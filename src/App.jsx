77e92a83e54c762108cb764f3579f0c26654b59a
dreampasssser <dreampasssser@foxmail.com>
2023-07-15 17:50:06 +0800

    将项目从cra迁移到vite。

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import { useState } from 'react';
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ squares, onClick }) {
+  const renderSquare = i => (
+    <Square value={squares[i]} onSquareClick={() => onClick(i)} />
+  );
+  return (
+    <>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const xIsNext = stepNumber % 2 === 0;
+  const currentSquares = history[stepNumber].slice();
+
+  const handleClick = i => {
+    if (calculateWinner(currentSquares) || currentSquares[i]) {
+      return;
+    }
+    currentSquares[i] = xIsNext ? 'X' : 'O';
+    setHistory([...history.slice(), currentSquares]);
+    setStepNumber(stepNumber + 1);
+  };
+
+  const jumpTo = step => {
+    setStepNumber(step);
+  };
+
+  const winner = calculateWinner(currentSquares);
+
+  const moves = history.map((squares, move) => {
+    const desc = move ? 'Go to move #' + move : 'Go to game start';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={currentSquares} onClick={i => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
