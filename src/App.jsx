ab18b6f8c81f79a212c311127219f0f7e5bceb45
dreampasssser <dreampasssser@foxmail.com>
2023-08-19 21:15:30 +0800

    教程后的练习。

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,91 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
-function Square({ value, onSquareClick }) {
+function Square({ shouldHighLight, value, onSquareClick }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button
+      className={'square' + (shouldHighLight ? ' highlight' : '')}
+      onClick={onSquareClick}
+    >
       {value}
     </button>
   );
 }
 
 function Board({ xIsNext, squares, onPlay }) {
-  const handleClick = i => {
-    if (calculateWinner(squares) || squares[i]) {
+  const handleClick = (i, position) => {
+    if (calculateWinner(squares).winner || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? 'X' : 'O';
-    onPlay(nextSquares);
+    onPlay(nextSquares, position);
   };
 
-  const renderSquare = i => (
-    <Square value={squares[i]} onSquareClick={() => handleClick(i)} />
-  );
+  const renderSquare = (i, j, winLine) => {
+    const index = 3 * i + j;
+    const position = [i, j];
+    return (
+      <Square
+        key={index}
+        shouldHighLight={winLine.includes(index)}
+        value={squares[index]}
+        onSquareClick={() => handleClick(index, [...position])}
+      />
+    );
+  };
 
-  const winner = calculateWinner(squares);
+  const renderBoard = winLine => {
+    const board = [];
+    for (let i = 0; i < 3; i++) {
+      const boardRow = [];
+      for (let j = 0; j < 3; j++) {
+        boardRow.push(renderSquare(i, j, winLine));
+      }
+      board.push(
+        <div key={`row-${i}`} className="board-row">
+          {boardRow}
+        </div>
+      );
+    }
+    return board;
+  };
+
+  const { winner, winLine } = calculateWinner(squares);
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
-  } else {
+  } else if (squares.some(square => square === null)) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  } else {
+    status = 'The game ended in a draw.';
   }
+  const board = renderBoard(winLine);
 
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
+      {board}
     </>
   );
 }
 
 export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [history, setHistory] = useState([
+    {
+      squares: Array(9).fill(null),
+      position: Array(2).fill(0),
+    },
+  ]);
   const [stepNumber, setStepNumber] = useState(0);
+  const [ascOrder, setAscOrder] = useState(true);
   const xIsNext = stepNumber % 2 === 0;
-  const currentSquares = history[stepNumber].slice();
+  const currentSquares = history[stepNumber].squares.slice();
+  const orderTxt = ascOrder
+    ? '升序（点击切换为降序）'
+    : '降序（点击切换为升序）';
 
-  const handlePlay = squares => {
-    setHistory([...history.slice(), squares]);
+  const handlePlay = (squares, position) => {
+    setHistory([...history.slice(), { squares, position }]);
     setStepNumber(stepNumber + 1);
   };
 
@@ -67,14 +93,27 @@ export default function Game() {
     setStepNumber(step);
   };
 
-  const moves = history.map((squares, move) => {
-    const desc = move ? 'Go to move #' + move : 'Go to game start';
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{desc}</button>
-      </li>
-    );
-  });
+  const handleOrder = () => {
+    setAscOrder(!ascOrder);
+  };
+
+  const moves = useMemo(
+    () =>
+      history
+        .map((value, move) => ({ ...value, move }))
+        .sort((a, b) => (ascOrder ? a.move - b.move : b.move - a.move))
+        .map(({ move, position }) => {
+          const desc = move
+            ? `Go to move #${move} (${position[0]}, ${position[1]})`
+            : 'Go to game start';
+          return (
+            <li key={move}>
+              <button onClick={() => jumpTo(move)}>{desc}</button>
+            </li>
+          );
+        }),
+    [history, ascOrder]
+  );
 
   return (
     <div className="game">
@@ -82,6 +121,10 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
+        <div>You are at move #{stepNumber}</div>
+        <div>
+          <button onClick={handleOrder}>{orderTxt}</button>
+        </div>
         <ol>{moves}</ol>
       </div>
     </div>
@@ -102,8 +145,8 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return { winner: squares[a], winLine: lines[i] };
     }
   }
-  return null;
+  return { winner: null, winLine: [] };
 }
